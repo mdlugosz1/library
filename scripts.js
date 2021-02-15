5f0ed7e70949ba9de6376b88eb2bfc92e8050c49
Michał Długosz <mdugosz@gmail.com>
2021-02-15 18:02:51 +0100

    Now user is able to add own book thorugh form

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
-const shelf = document.querySelector('.shelf');
+const form = document.querySelector('form');
+const addButton = document.querySelector('.new-book');
+const tableBody = document.querySelector('tbody');
+const submit = document.querySelector('.submit');
 
 let myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(author, title, pages, read, buttons) {
     this.author = author
     this.title = title
     this.pages = pages
     this.read = read
+    this.buttons = buttons
 }
 
 function addBookToLibrary() {
-    myLibrary.push(book1);
-    myLibrary.push(book2);
-    myLibrary.push(book3);
+   let formAuthor = document.querySelector('#author').value;
+   let formTitle = document.querySelector('#title').value;
+   let formPages = document.querySelector('#pages').value;
+
+   const addedBook = new Book(formAuthor, formTitle, formPages);
+   
+   myLibrary.push(addedBook);
+   formAuthor.value = '';
 }
 
-const book1 = new Book('King', 'Cos tam', 150, false);
-const book2 = new Book('Tolkien', 'Lord of the Rings', 1000, true);
-const book3 = new Book('Tolkien', 'Hobbit', 1000, true);
-
-addBookToLibrary();
-
-function displayBooks(arr) {
+function displayBooks(library) {
     
 }
 
 displayBooks(myLibrary);
 
+addButton.addEventListener('click', () => {
+    form.classList.add('display-form');
+})
 
+submit.addEventListener('click', () => {
+    addBookToLibrary();
+    displayBooks(myLibrary);
+});
