d20efe4bf1f0fd63fa8bb6bc5f76a2163212df6e
Michał Długosz <mdugosz@gmail.com>
2021-02-16 20:08:23 +0100

    Added ability to remove books from library

--- scripts.js
+++ scripts.js
@@ -5,12 +5,11 @@ const submit = document.querySelector('.submit');
 
 let myLibrary = [];
 
-function Book(author, title, pages, read, buttons) {
+function Book(author, title, pages, read) {
     this.author = author
     this.title = title
     this.pages = pages
     this.read = read
-    this.buttons = buttons
 }
 
 function addBookToLibrary() {
@@ -22,8 +21,6 @@ function addBookToLibrary() {
     const addedBook = new Book(formAuthor, formTitle, formPages, formIsRead);
     
     myLibrary.push(addedBook);
-
-    tableBody.innerHTML = '';
 }
 
 //Function checks if button is checked and then returns it's value
@@ -41,18 +38,48 @@ function radioButtonsCheck(form, name) {
 }
 
 function displayBooks(library) {   
+    tableBody.innerHTML = '';
+
     library.forEach(book => {
         const newRow = document.createElement('tr');
         tableBody.appendChild(newRow);
+        newRow.setAttribute('data', library.indexOf(book));
         
         for (let key in book) {
             const newCell = document.createElement('td');
             newRow.appendChild(newCell);
             newCell.textContent = book[key];
         }
+
+        const removeButton = document.createElement('button');
+        const cellForButtons = document.createElement('td');
+        removeButton.textContent = 'remove';
+        removeButton.value = library.indexOf(book);
+        
+        cellForButtons.appendChild(removeButton);
+        newRow.appendChild(cellForButtons);
+
+        removeButton.addEventListener('click', removeBook);
     });
 }
 
+function removeBook(e) {
+    const buttonValue = e.target.value;
+    const rowToRemove = tableBody.querySelectorAll('tr');
+
+    for (let i = 0; i <= rowToRemove.length; i++) {
+        if (rowToRemove[i].getAttribute('data') === buttonValue) {
+            myLibrary.splice(i, 1);
+            break;
+        }
+    }
+
+    displayBooks(myLibrary);
+}
+
+function editBook(e) {
+    
+}
 
 addButton.addEventListener('click', () => {
     form.classList.add('display-form');
@@ -61,4 +88,5 @@ addButton.addEventListener('click', () => {
 submit.addEventListener('click', () => {
     addBookToLibrary();
     displayBooks(myLibrary);
+    form.classList.remove('display-form');
 });
